bb62223c8cb3560cef1df75769fbd1b428541ce3
Yakov <nevidomskiy8@gmail.com>
2021-04-24 23:37:46 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {LandingScreen} from './src/screens/LandingScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <LandingScreen/>
     </View>
   );
 }
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ src/screens/LandingScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const LandingScreen = () => {
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.navigation}>
+            <Text>Navigation</Text>
+        </View>
+        <View style={styles.body}>
+            <Text>body</Text>
+        </View>
+        <View style={styles.footer}>
+            <Text>Footer</Text>
+        </View>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "green"
+    },
+    navigation: {
+        flex: 2,
+        backgroundColor: "red"
+    },
+    body: {
+        flex: 9,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "yellow"
+    }, 
+    footer: {
+        flex: 1,
+        backgroundColor: "cyan"
+    }
+})
